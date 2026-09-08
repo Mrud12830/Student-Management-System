3e533b72ca06ce49fcb8c25d72d806e6c8dee91a
Mrudula Bidwai <mrudulabidwai30@gmail.com>
2026-09-08 23:59:39 +0530

    Secure database credentials and ignore compiled files

--- DBConnection.java
+++ DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
 
     private static final String USER = "root";
 
-    private static final String PASSWORD = "Pradnya@1970";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
